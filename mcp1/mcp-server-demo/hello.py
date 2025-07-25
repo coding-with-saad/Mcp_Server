13f5c57fd78db6905424d297ccf80bc9daa0e7bc
saadkhawar15 <saadkhawar169@gmail.com>
2025-07-25 16:50:15 +0500

    add spec format references to MCP server fundamental step

--- mcp1/mcp-server-demo/hello.py
+++ mcp1/mcp-server-demo/hello.py
@@ -1,11 +1,11 @@
 from mcp.server.fastmcp import FastMCP
 mcp=FastMCP(name="hello-mcp", stateless_http=True)
-@mcp.tool()
+@mcp.tool(name="online_resercher",description="search the web information")
 def search_online(query:str) ->str:
     return f"Results for {query}..."
-@mcp.tool()
-async def get_weather(city:str) ->str:
-    return f"weather in {city} is sunny..."
+# @mcp.tool()
+# async def get_weather(city:str) ->str:
+#     return f"weather in {city} is sunny..."
 
 
 mcp_app=mcp.streamable_http_app()
